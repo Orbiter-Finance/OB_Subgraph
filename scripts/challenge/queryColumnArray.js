ade4a8aa930ebb5814bb18cfa4fc83aa51b6a818
Pis <piscarefree@gmail.com>
2023-12-12 11:59:21 +0800

    update script

--- scripts/challenge/queryColumnArray.js
+++ scripts/challenge/queryColumnArray.js
@@ -7,7 +7,7 @@ const maker = '0x227d76ab1cea2edfc9a62833af1743259c1f055f';
 const mdcAddress = '0xaabe9fec8ad02ccf32f1433bc013eb812948498a';
 const txTimestamp = '1701761838';
 
-//purose: serach ColumnArray value
+//purose1: serach ColumnArray value
 axios
   .post(testAPI, {
     query: `
@@ -27,6 +27,47 @@ axios
             chainIds
         }
     }
+    `,
+  })
+  .then((result) => {
+    for (const resultGet of result.data.data.columnArraySnapshots) {
+      console.log('columnArray:', resultGet);
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//purose2: serach latest index
+axios
+  .post(testAPI, {
+    query: `
+    {
+      columnArraySnapshots(
+          where: {
+              mdc_: {
+                  id: "${mdcAddress}"
+                  owner: "${maker}"
+              }
+          }
+          orderBy: enableTimestamp
+          orderDirection: desc
+          first: 1
+      ) {
+        dealerMappingSnapshot(orderBy: dealerIndex, orderDirection: asc) {
+            dealerAddr
+            dealerIndex
+        }
+        ebcMappingSnapshot(orderBy: ebcIndex, orderDirection: asc) {
+            ebcAddr
+            ebcIndex
+        }
+        chainIdMappingSnapshot(orderBy: chainIdIndex, orderDirection: asc) {
+            chainId
+            chainIdIndex
+        }
+    }
+  }
     `,
   })
   .then((result) => {
